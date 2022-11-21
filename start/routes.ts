0b33709d271e7add6f1794c9606a8417bb638cdc
mitchellb613 <mkb.061397@gmail.com>
2022-11-21 17:21:33 -0600

    sketch application routes

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,35 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
+
+Route.get('/user/home', () => {
+  return 'Render view of user home with their tarantulas in a table'
+})
+
+Route.get('/user/signup', () => {
+  return 'Render form for user signup'
+})
+
+Route.post('/user/signup', () => {
+  return 'Post route for user signup'
+})
+
+Route.get('/user/login', () => {
+  return 'Render form for user login'
+})
+
+Route.post('/user/login', () => {
+  return 'Post route for user login'
+})
+
+Route.get('/tarantula/create', () => {
+  return 'Render form for creating new tarantula'
+})
+
+Route.post('/tarantula/create', () => {
+  return 'Post route for making new tarantula'
+})
+
+Route.get('/tarantula/view/:id', () => {
+  return 'Render view of tarantula with id'
+})
