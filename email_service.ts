eee293d895b2cbdaac6cf2aebd79073fb1fbb90e
mitchellb613 <mkb.061397@gmail.com>
2022-11-21 15:41:19 -0600

    initial

---
+++ email_service.ts
@@ -0,0 +1,15 @@
+import { DateTime } from 'luxon'
+import ReminderEmailfrom 'App/Mailers/ReminderEmail'
+
+public async run () {
+  // Get users to remind
+  const users = await User.query()
+    .where('next_reminder_time', >=, DateTime.now().set({minute: 0, second: 0, millisecond: 0}).toSQL())
+  // Send out emails
+  for (const user of users) {
+    await new ReminderEmail(user).send()
+    // Set 'reminder_sent_at' so it repeats
+    user.next_reminder_time = DateTime.now().plus({days: user.ReminderIntervalDays})
+    await user.save()
+  }
+}
