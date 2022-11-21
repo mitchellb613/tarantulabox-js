4c480738e7729302f565f2b563794e1cb5a5b57b
mitchellb613 <mkb.061397@gmail.com>
2022-11-21 15:47:14 -0600

    change to lte now()

--- email_service.ts
+++ email_service.ts
@@ -4,7 +4,7 @@ import ReminderEmailfrom 'App/Mailers/ReminderEmail'
 public async run () {
   // Get users to remind
   const users = await User.query()
-    .where('next_reminder_time', >=, DateTime.now().set({minute: 0, second: 0, millisecond: 0}).toSQL())
+    .where('next_reminder_time', <=, DateTime.now().set({minute: 0, second: 0, millisecond: 0}).toSQL())
   // Send out emails
   for (const user of users) {
     await new ReminderEmail(user).send()
