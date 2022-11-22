6c8ab245aadbc92883e5224ee34eab2b19a02f96
mitchellb613 <mkb.061397@gmail.com>
2022-11-21 23:46:42 -0600

    add auth middleware

--- start/routes.ts
+++ start/routes.ts
@@ -24,10 +24,6 @@ Route.get('/', async ({ view }) => {
   return await view.render('home')
 })
 
-Route.get('/user/home', async () => {
-  return 'Render view of user home with their tarantulas in a table'
-})
-
 Route.get('/user/signup', 'SignupController.index')
 
 Route.post('/user/signup', 'SignupController.post')
@@ -36,20 +32,26 @@ Route.get('/user/login', 'LoginController.index')
 
 Route.post('/user/login', 'LoginController.post')
 
-Route.post('/user/logout', async ({ auth, response, session }) => {
-  session.flash('global_message', 'Logged out successfully')
-  await auth.use('web').logout()
-  response.redirect('/')
-})
+Route.group(() => {
+  Route.post('/user/logout', async ({ auth, response, session }) => {
+    session.flash('global_message', 'Logged out successfully')
+    await auth.use('web').logout()
+    response.redirect('/')
+  })
 
-Route.get('/tarantula/create', async () => {
-  return 'Render form for creating new tarantula'
-})
+  Route.get('/user/home', async () => {
+    return 'Render view of user home with their tarantulas in a table'
+  })
 
-Route.post('/tarantula/create', async () => {
-  return 'Post route for making new tarantula'
-})
+  Route.get('/tarantula/create', async () => {
+    return 'Render form for creating new tarantula'
+  })
 
-Route.get('/tarantula/view/:id', async () => {
-  return 'Render view of tarantula with id'
-})
+  Route.post('/tarantula/create', async () => {
+    return 'Post route for making new tarantula'
+  })
+
+  Route.get('/tarantula/view/:id', async () => {
+    return 'Render view of tarantula with id'
+  })
+}).middleware('auth:web')
