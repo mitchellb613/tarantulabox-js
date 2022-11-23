85f125020d7a9eaf41bdc625866bd06b087c4b3f
mitchellb613 <mkb.061397@gmail.com>
2022-11-23 00:55:16 -0600

    wrap DashboardController in trycatch

--- app/Controllers/Http/DashboardController.ts
+++ app/Controllers/Http/DashboardController.ts
@@ -4,20 +4,24 @@ import User from 'App/Models/User'
 
 export default class DashboardController {
   public async index(ctx: HttpContextContract) {
-    const user = await User.find(ctx.auth.user?.id)
-    const tarantulas = await user?.related('tarantulas').query()
-    if (!tarantulas) {
+    try {
+      const user = await User.find(ctx.auth.user?.id)
+      const tarantulas = await user?.related('tarantulas').query()
+      if (!tarantulas) {
+        return ctx.response.internalServerError('500')
+      }
+      const tarantulas_signed = await Promise.all(
+        tarantulas.map(async (t) => {
+          const signed_url = await Drive.getSignedUrl(t.img_url)
+          return {
+            ...t.$attributes,
+            signed_url: signed_url,
+          }
+        })
+      )
+      return await ctx.view.render('dashboard', { tarantulas: tarantulas_signed })
+    } catch (error) {
       return ctx.response.internalServerError('500')
     }
-    const tarantulas_signed = await Promise.all(
-      tarantulas.map(async (t) => {
-        const signed_url = await Drive.getSignedUrl(t.img_url)
-        return {
-          ...t.$attributes,
-          signed_url: signed_url,
-        }
-      })
-    )
-    return await ctx.view.render('dashboard', { tarantulas: tarantulas_signed })
   }
 }
