e0e6acbd8f88d9025beb720d3723feeeb13e93f2
mitchellb613 <mkb.061397@gmail.com>
2022-11-23 21:58:13 -0600

    remove seconds from email_service it is optional

--- email_service.ts
+++ email_service.ts
@@ -4,7 +4,6 @@ import { spawn } from 'child_process'
 const rule = new ns.RecurrenceRule()
 rule.hour = new ns.Range(6, 18)
 rule.minute = 0
-rule.second = 0
 
 ns.scheduleJob(rule, () => {
   spawn('node', ['ace', 'send:reminder'])
